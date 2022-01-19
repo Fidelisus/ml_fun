6129641518800cb472722a522399738285fee09d
Łukasz Sobocinski <l.sobocinski123@gmail.com>
2022-01-19 14:00:59 +0100

    Add learning

--- Exercise 3/src/mc_agent.py
+++ Exercise 3/src/mc_agent.py
@@ -1,22 +1,15 @@
 
 import random
-
-
+import pickle
 
 from gym_env import TicTacToeEnv, set_log_level_by, agent_by_mark,\
     next_mark, check_game_status, after_action_state, O_REWARD, X_REWARD
 
-
 DEFAULT_VALUE = 0
-EPISODE_COUNT = 50000
-
-
-Q = {}
 
 default_actions = {0: DEFAULT_VALUE, 1: DEFAULT_VALUE, 2: DEFAULT_VALUE, 3: DEFAULT_VALUE, 4: DEFAULT_VALUE, 5: DEFAULT_VALUE, 6: DEFAULT_VALUE, 7: DEFAULT_VALUE, 8: DEFAULT_VALUE}
 
 set_log_level_by(0)
-
 
 def get_max_indices(vals):
     m = max(vals)
@@ -26,26 +19,19 @@ def get_min_indices(vals):
     m = min(vals)
     return [i for i, v in enumerate(vals) if v == m]
 
-
 class MCAgent(object):
-    def __init__(self, mark, alpha, epsilon):
+    def __init__(self, mark, alpha, epsilon, Q):
         self.mark = mark
         self.alpha = alpha
         self.epsilon = epsilon
-
-
+        self.Q = Q
 
     def act(self, state, actions):
         return self.egreedy(state, actions)
 
-
-
     def egreedy(self, state, actions):
-        
-
-        if not state in Q:
-            Q[state] = default_actions.copy()
-
+        if not state in self.Q:
+            self.Q[state] = default_actions.copy()
 
         r = random.random()
         if r < self.epsilon:
@@ -53,46 +39,35 @@ class MCAgent(object):
         else:
             return self.get_best_action(state, actions)
 
-
-
     def get_random_action(self, actions):
         return random.choice(actions)
 
     def get_best_action(self, state, actions):
-        "Returns the best action given a current state and available actions"
         values = []
 
         for action in actions:
             action_val = self.get_state_val(state, action)
             values.append(action_val)
 
-
         if self.mark == 'O':
             indices = get_max_indices(values)
         else:
             indices = get_min_indices(values)
 
-        return random.choice(indices)
-
+        i = random.choice(indices)
+        return actions[i]
 
     def get_state_val(self, state, action):
-        return Q[state][action]
-    
+        return self.Q[state][action]
 
-
-
-
-def learn(alpha, epsilon):
-    global Q
-    Q = {}
-
+def learn(alpha, epsilon, episode_count, Q):
     env = TicTacToeEnv()
-    agents = [MCAgent('O', alpha, epsilon),
-                MCAgent('X', alpha, epsilon)]
-    
+    agents = [MCAgent('O', alpha, epsilon, Q),
+                MCAgent('X', alpha, epsilon, Q)]
+
     start_mark = 'O'
     reward_list = []
-    for i in range(EPISODE_COUNT):
+    for i in range(episode_count):
 
         env.show_episode(False, i)
         env.set_start_mark(start_mark)
@@ -105,12 +80,9 @@ def learn(alpha, epsilon):
             actions = env.available_actions()
             action = agent.act(state, actions)
 
-            env.show_turn(False, mark)
+            state_action_list.append((state, action))
 
-            if mark == 'O':
-                state_action_list.append((state, action))
-
-            state, reward, done, info = env.step(action)
+            state, reward, done, _ = env.step(action)
             state, mark = state
         
         env.show_result(False, mark, reward)
@@ -119,17 +91,25 @@ def learn(alpha, epsilon):
 
         reward_list.append(reward)
         start_mark = next_mark(start_mark)
-    print(reward_list[-10:])
+    return reward_list
 
+def evaluate_agent():
+    with open('Exercise 3/q_array_dumps/perfect_model.pkl', 'rb') as f:
+        Q = pickle.load(f)
 
-learn(0.3, 0.1)
+    episode_count = 1000
 
+    reward_list = learn(0, 0, episode_count, Q)
+    print("Reward list: ", reward_list[0:100])
+    print(reward_list.count(0))
+    print("% of non-draw games", (episode_count-reward_list.count(0))/episode_count)
 
-            
+episode_count = 50000
+Q = {}
 
+learn(0.5, 0.05, episode_count, Q)
 
+with open('Exercise 3/q_array_dumps/perfect_model.pkl', 'wb') as f:
+    pickle.dump(Q, f)
 
-
-
-
-    
+evaluate_agent()
